0ad0b584055fd37aceb399d4fd395ca4b49a87f8
jamesate4e <james.chinwokwu@e4email.net>
2021-08-24 11:24:19 +0100

    add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,30 @@
 <template>
-  <SearchBar></SearchBar>
-  <my-header></my-header>
-
-  <div class="container-fluid">
-    <h1 class="text-uppercase">Event ({{ events.length }})</h1>
-    <div class="grip container-fluid">
-      <EventCard
-        v-for="(item, index) in events"
-        :key="index"
-        :eventDetails="item"
-      ></EventCard>
-    </div>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
-
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <router-view />
 </template>
 
-<script>
-import MyHeader from "./components/SearchBar.vue";
-import SearchBar from "./components/MyHeader.vue";
-import EventCard from "./components/EventCard.vue";
-import data from "@/assets/data.json";
-
-export default {
-  name: "App",
-  data() {
-    return {
-      events: data,
-    };
-  },
-  components: {
-    MyHeader,
-    SearchBar,
-    EventCard,
-  },
-};
-</script>
-
 <style lang="scss">
-* {
-  margin: 0px;
-  padding: 0px;
-  font-family: "Almarai", sans-serif;
-  box-sizing: border-box;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-.text-uppercase {
-  text-transform: uppercase;
-}
+#nav {
+  padding: 30px;
 
-.container-fluid {
-  width: 100%;
-  padding-right: 0.75rem;
-  padding-left: 0.75rem;
-  margin-right: auto;
-  margin-left: auto;
-  max-width: 1320px;
-}
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-.grip {
-  display: grid;
-  gap: 32px;
-  grid-template-columns: repeat(3, 1fr);
-
-  @media (max-width: 780px) {
-    grid-template-columns: repeat(1, 1fr);
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
 }
 </style>
